37386426b3b7c6fa1efa11db3c73eb832838352c
daanyaalhussain <92949495+daanyaalhussain@users.noreply.github.com>
2021-11-28 22:03:42 -0500

    Update window.py

--- DaanyaalHussain/window.py
+++ DaanyaalHussain/window.py
@@ -1,6 +1,14 @@
 import pygame
 print("works")
-window = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((500,500))
 pygame.display.flip()
+pygame.display.set_caption("Interface")
+pygame.font.init()
+font = pygame.font.SysFont('aerial', 40)
+
+screen.blit(font.render('Daanyaal Hussain', True, (250,250,0)), (0,0))
+screen.blit(font.render('Grade 9.', True, (0,250,0)), (0,30))
+screen.blit(font.render('Daanyaal Hussain is a Freshman.', True, (0,0,250)), (0,60))
+pygame.display.update()
 while True:
   pass
